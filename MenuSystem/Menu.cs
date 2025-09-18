3300b66369497b6d65d0f9003eba7925e26b2f39
Marcus Will <Marcuswill5974@gmail.com>
2025-09-18 10:53:07 +0200

    Første commit

---
+++ MenuSystem/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    internal class Menu
+    {
+        public string Title;
+        public MenuItem[] MenuItems;
+        public int ItemCount = 0;
+        public void Show()
+        {
+            Console.WriteLine(Title);
+            for (int i = 0; i < ItemCount; i++)
+            {
+                Console.WriteLine(MenuItems[i].Title);
+            }
+
+        }
+        
+        
+
+    }
+}
---
+++ øvelser 18-09/Menusystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace øvelser_18_09
+{
+    internal class Menusystem
+    {
+        public class Title;
+        public class MenuItems;
+        public class ItemCount;
+        public class show();
+    }
+}
